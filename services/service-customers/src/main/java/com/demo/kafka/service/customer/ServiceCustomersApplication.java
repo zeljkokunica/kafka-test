6ac784a1e930b3c73778abf8e8e50c29002b2780
zeljkokunica <zeljko.kunica@engagetech.com>
2019-07-05 10:00:59 +0200

    initial

---
+++ services/service-customers/src/main/java/com/demo/kafka/service/customer/ServiceCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.service.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCustomersApplication.class, args);
+    }
+
+}
